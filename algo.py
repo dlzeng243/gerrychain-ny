3f56a174db791087d31d775c86844c47df8cf171
Brad Zhang <bwzhang@andrew.cmu.edu>
2022-05-07 16:06:49 -0400

    added splitting constraint, organized folder/code

--- algo.py
+++ algo.py
@@ -11,6 +11,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def main():
+    map_name = 'ny_cong_2022_invalidated_precincts'
 
     # initialize the graph
     graph = Graph.from_file("./map_data/tl_2020_36_vtd20.zip")
@@ -24,7 +25,7 @@ def main():
     graph.join(df, None, "GEOID20", "GEOID20vtd")
 
     # import initial district assignments
-    df = pd.read_csv("ny_cong_2022_invalidated.csv")
+    df = pd.read_csv(f"precincts/{map_name}.csv")
     df["GEOID20"] = df["GEOID20"].astype(str)
     graph.join(df, None, "GEOID20", "GEOID20")
 
@@ -41,16 +42,24 @@ def main():
     )
 
     ideal_population = sum(initial_partition['population'].values())/len(initial_partition)
-    proposal = partial(recom, pop_col='pop', pop_target=ideal_population, epsilon=0.0075, node_repeats=2)
+    proposal = partial(recom, pop_col='pop', pop_target=ideal_population, epsilon=0.02, node_repeats=2)
     compactness_bound = constraints.UpperBound(
         lambda p: len(p["cut_edges"]),
         2*len(initial_partition["cut_edges"])
     )
-    pop_constraint = constraints.within_percent_of_ideal_population(initial_partition, 0.0075)
+    pop_constraint = constraints.within_percent_of_ideal_population(initial_partition, 0.02)
+    def check_split(edge):
+        if graph.nodes[edge[0]]["county"] == graph.nodes[edge[1]]["county"]:
+            return graph.nodes[edge[0]]["county"]
+        return None
+    splitting_constraint = constraints.UpperBound(
+        lambda p: len(set(map(check_split, p["cut_edges"]))),
+        35
+    )
 
     chain = MarkovChain(
         proposal=proposal,
-        constraints=[compactness_bound, pop_constraint, constraints.contiguous],
+        constraints=[compactness_bound, pop_constraint, constraints.contiguous, splitting_constraint],
         accept=always_accept,
         initial_state=initial_partition,
         total_steps=100
@@ -59,11 +68,11 @@ def main():
     # run the algorithm and output data
     d_percents = [sorted(partition["CLN20"].percents("Dem")) for partition in chain]
     data = pd.DataFrame(d_percents)
-    data.to_csv("single_flip.csv")
+    data.to_csv(f"district_assignments/{map_name}_simulation_data.csv")
     
     assignment_dict = chain.state.assignment
     districts = pd.DataFrame(data=[(graph.nodes[k]["GEOID20"], assignment_dict[k]) for k, _ in enumerate(assignment_dict)], columns=["GEOID20", "District"])
-    districts.to_csv("district_assignments.csv", index=False)
+    districts.to_csv(f"district_assignments/{map_name}_district_assignments.csv", index=False)
 
     fig, ax = plt.subplots(figsize=(8, 6))
 
@@ -83,6 +92,7 @@ def main():
     ax.set_ylim(0, 1)
     ax.set_yticks([0, 0.25, 0.5, 0.75, 1])
 
+    plt.savefig(f"district_assignments/{map_name}_plot.png", bbox_inches='tight')
     plt.show()
 
 if __name__=="__main__":
